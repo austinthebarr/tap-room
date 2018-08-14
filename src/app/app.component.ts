5bb593cc3ffbfede1d29c2cb937164c70c114e24
Austin Barr and Hisato Kawaminami <hisato@>
2018-08-14 11:48:29 -0700

    add edit and done buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  kegs: Keg[] =[
+    new Keg('Lager', 'Brooklyn Lager', 'Brooklyn Brewery', 6, '5.2%'),
+    new Keg('IPA', 'Fremont IPA', 'FremontBrewing Company', 5, '6,2%'),
+    new Keg('Bavarian Bright Lager', 'Hello, World!','Optimism Brewing', 6, '5.3%'),
+  ]
+  sellPint(keg){
+    keg.pints--;
+    console.log(keg.pints)
+  }
+  selectedKeg = null;
+  editKeg(clickedKeg){
+    this.selectedKeg = clickedKeg;
+  }
+  finishedEditing(){
+    this.selectedKeg = null;
+  }
 }
